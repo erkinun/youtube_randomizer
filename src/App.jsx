f1a3cb80c214ab2568da350e1682d67928d5b744
erkinun <erkinun@gmail.com>
2023-02-16 12:41:20 +0300

    youtube component

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,9 @@
-import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import YouTube from 'react-youtube';
+import { YoutubePlayer } from './YouTubePlayer'
+
 
 function App() {
-  const [videos, setVideos] = useState([])
-
-  const opts = {
-    height: '390',
-    width: '640',
-    playerVars: {
-      // https://developers.google.com/youtube/player_parameters
-      autoplay: 1,
-    },
-  };
-
-  function _onReady(event) {
-    // access to player in all event handlers via event.target
-    event.target.pauseVideo();
-  }
-
-
-
-  function pickRandomVideo() {
-    if (videos.length === 0) return 'otV4xSdXLh0';
-
-    const randomVideo = videos[Math.floor(Math.random() * videos.length)];
-    return randomVideo
-  }
-
-  useEffect(() => {
-    async function fetchRandomVideos() {
-      const videosResult = await fetch(`https://youtube.googleapis.com/youtube/v3/search?maxResults=50&q=funny_birds&key=${import.meta.env.VITE_API_KEY}`)
-      const videos = await videosResult.json()
-      const videoIds = videos.items?.map(video => video.id.videoId);
-      setVideos(videoIds)
-    }
-    fetchRandomVideos()
-  }, [])
-
-
 
   return (
     <div className="App">
@@ -52,9 +16,7 @@ function App() {
         </a>
       </div>
       <h1>Youtube player</h1>
-      <div className="card">
-        <YouTube videoId={pickRandomVideo()} opts={opts} onReady={_onReady} />
-      </div>
+      <YoutubePlayer />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
